cd50b0b28ffd2858d6798468ec5c4f8e01dc8383
Anderson R soares <anderson.r.soares@gmail.com>
2017-03-09 18:00:51 -0300

    lista de itens

--- app4/index.android.js
+++ app4/index.android.js
@@ -1,41 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry
 } from 'react-native';
 
-import ListaItens from './src/components/listaItens'
+import ListaItens from './src/components/ListaItens';
 
-
-
-export default class app4 extends Component {
-
-    constructor(props){
-      super(props);
-      console.log('construindo aplicacao');
-    }
-
-    componentWillMount(){
-        console.log('fazer alguma coisa antes de renderizar');
-    }
-
-    componentDidMount(){
-      console.log('fazer alguma coisa depois de renderizar');
-    }
-
-
-    render() {
-        console.log('objeto é renderizado');
-      return (
-        <ListaItens/>
-      );
+class app4 extends Component {
+  render() {
+    return (
+      <ListaItens />
+    );
   }
 }
-
 
 AppRegistry.registerComponent('app4', () => app4);
--- app4/src/components/listaItens.js
+++ app4/src/components/listaItens.js
@@ -1,43 +1,30 @@
-/**
- * Created by DevMaker on 3/9/17.
- */
-
 import React, { Component } from 'react';
 import {
-    Text,
-    View
+  ScrollView
 } from 'react-native';
-
 import axios from 'axios';
-import Itens from './itens';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
 
+	constructor(props) {
+		super(props);
 
-    constructor(props){
-        super(props);
-        this.state = {listaItens:[]};
-    }
+		this.state = { listaItens: [] };
+	}
 
-    componentWillMount(){
-        //
-        axios.get("http://faus.com.br/recursos/c/dmairr/api/itens.html")
-            .then(response => {
-                console.log(response);
-                this.setState( {listaItens:response.data});
-            })
-            .catch((error)=>{
-                console.log(error);
-            });
-    }
+	componentWillMount() {
+		//requisção HTTP
+		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			.then(response => { this.setState({ listaItens: response.data }); })
+			.catch(() => { console.log('Erro ao recuperar os dados'); });
+	}
 
-    render() {
-        return (
-            <View>
-                    {this.state.listaItens.map(item=> { return(<Text key={item.titulo}>{item.titulo}</Text>);})
-                }
-            </View>
-        );
-    }
+  render() {
+    return (
+			<ScrollView style={{ backgroundColor: '#DDD' }}>
+				{ this.state.listaItens.map(item => (<Itens key={item.titulo} item={item} />))}
+			</ScrollView>
+    );
+  }
 }
-
