10db05518a37123334e216f8c4292ea122f50bf5
Anderson R soares <anderson.r.soares@gmail.com>
2017-03-09 17:28:12 -0300

    itens

--- app4/src/components/listaItens.js
+++ app4/src/components/listaItens.js
@@ -8,15 +8,34 @@ import {
     View
 } from 'react-native';
 
+import axios from 'axios';
 import Itens from './itens';
 
 export default class ListaItens extends Component {
+
+
+    constructor(props){
+        super(props);
+        this.state = {listaItens:[]};
+    }
+
+    componentWillMount(){
+        //
+        axios.get("http://faus.com.br/recursos/c/dmairr/api/itens.html")
+            .then(response => {
+                console.log(response);
+                this.setState( {listaItens:response.data});
+            })
+            .catch((error)=>{
+                console.log(error);
+            });
+    }
+
     render() {
         return (
             <View>
-                <Itens/>
-                <Itens/>
-                <Itens/>
+                    {this.state.listaItens.map(item=> { return(<Text key={item.titulo}>{item.titulo}</Text>);})
+                }
             </View>
         );
     }
